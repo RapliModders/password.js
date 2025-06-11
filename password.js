5c5138bf921a1395bc4e16eb78deafc1db78853c
RapliModders <segelehbostehz@gmail.com>
2025-06-11 16:05:12 +0700

    Update key to corzzaganteng

--- password.js
+++ password.js
@@ -1,17 +1,3 @@
 {
-
-  "secretCode": "terong",
-
-  "description": "XTRVLN", 
-
-  "lastUpdated": "12/06/2025",
-
-  "author": "RAPLIWITHCORZA", 
-
-  "status": "Active",
-
-  "license": "MIT",
-
-  "version": "6.0"
-
+  "key": "corzzaganteng"
 }
