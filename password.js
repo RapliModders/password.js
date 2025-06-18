6e09255dbe564c2eaf24f7ea6a79df0be8ec38dc
RapliModders <segelehbostehz@gmail.com>
2025-06-18 19:40:22 +0700

    Update password.js

--- password.js
+++ password.js
@@ -1,3 +1,17 @@
 {
-  "key": "corzzaganteng"
+
+  "secretCode": "LubzyV30",
+
+  "description": "CrashForCrasherLast!", 
+
+  "lastUpdated": "18/06/2025",
+
+  "author": "RapplixLubzy", 
+
+  "status": "Active",
+
+  "license": "MIT",
+
+  "version": "30.0"
+
 }
